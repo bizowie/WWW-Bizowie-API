009293651bf7cb2b7d9f6e54dbb378101ddc7465
Michael J. Flickinger <mjflick@gnu.org>
2026-05-12 14:41:00 +0000

    update version

--- lib/WWW/Bizowie/API.pm
+++ lib/WWW/Bizowie/API.pm
@@ -9,7 +9,7 @@ use HTTP::Request::Common;
 use Try::Tiny;
 use JSON;
 
-our $VERSION = '0.03';
+our $VERSION = '0.05';
 
 =head1 NAME
 
@@ -23,11 +23,9 @@ WWW::Bizowie::API - Perl API interface to Bizowie's L<erp software|http://bizowi
       site       => 'mysite.bizowie.com',
   );
 
-  $bz->apiv2_call(
-      'databases/database/add_note', {
-          db_table_id => 7,
-          primary_key => 12,
-          comment     => "I added this comment via the API!",
+  $bz->call(
+      'databases/add_note/3/10/123', {
+          comment => "I added this comment via the API!",
       },
   );
 
@@ -39,7 +37,17 @@ Returns a new instance of WWW::Bizowie::API
 
 Requires three parameters: api_key (your Bizowie API key), secret_key, and site (the hostname of your Bizowie instance).
 
-If utilizing apiv2 functionality, api_version may also be passed.
+Optionally accepts:
+
+=over 4
+
+=item * v2 - if true, calls are routed through the v2 API endpoint (C</bz/apiv2/call/>) instead of the v1 endpoint (C</bz/api/>). Recommended for new integrations.
+
+=item * api_version - when v2 mode is enabled, specifies the API version sent with each request (defaults to C<'1.00'>).
+
+=item * debug - if true, warns with the raw HTTP response when a response cannot be decoded as JSON.
+
+=back
 
 =cut
 
@@ -48,7 +56,7 @@ sub new
     my ($class, %params) = @_;
 
     my $bzua = LWP::UserAgent->new;
-    $bzua->ssl_opts( verify_hostname => 0 ); 
+    $bzua->ssl_opts( verify_hostname => 0, SSL_verify_mode => 0 ); 
     $bzua->agent('Bizowie::API');
 
     die 'site not specified'    unless $params{site};
@@ -56,10 +64,12 @@ sub new
     die 'secret_key not specified' unless $params{secret_key};
 
     my $self = {
-        ua         => $bzua,
-        api_key    => $params{api_key},
-        secret_key => $params{secret_key},
-        site       => $params{site},
+        ua          => $bzua,
+        api_key     => $params{api_key},
+        secret_key  => $params{secret_key},
+        site        => $params{site},
+        v2          => $params{v2},
+        debug       => $params{debug},
         api_version => $params{api_version},
     };
 
@@ -68,60 +78,26 @@ sub new
     return $self;
 }
 
-=head2 apiv2_call
-
-Makes a call via a Bizowie apiv2 endpoint.
-
-Takes two a parameters: a string indicating the path to the API method you wish to call, and a has reference of the parameters to be passed.
-
-=cut
-
-sub apiv2_call
-{
-    my ($self, $method, $params) = @_;
-
-    my $site = $self->{site};
-
-    die "[Bizowie::API] fatal error: no method given" unless $method;
-
-    $params->{api_version} = $self->{api_version};
-    $params->{api_key}     = $self->{api_key};
-    $params->{secret_key}  = $self->{secret_key};
-
-    my $request = encode_json($params || { });
-    my $r = POST("https://${site}/bz/apiv2/call/$method",
-         Content_Type => 'form-data',
-         Content      => [
-             site       => $site,
-             request    => $request,
-         ]
-    );
-    my $q = $self->{ua}->request($r);
-    my $o;
-    {
-        local $SIG{__DIE__} = sub { };
-        try {
-            $o = decode_json($q->decoded_content);
-        } catch {
-            $o = { unprocessed => 1 };
-        };
-    }
-
-    return WWW::Bizowie::API::Response->new(
-        data    => $o,
-        success => delete $o->{success} || 0,
-    );
-}
-
 =head2 call
 
-Makes a Bizowie legacy API call.
+Makes a Bizowie API call.
 
-Takes two a parameters: a string indicating the path to the legacy API method you wish to call, and a has reference of the parameters to be passed.
+Takes two parameters: a string indicating the path to the API method you wish to call, and a hash reference of the parameters to be passed.
+
+If the object was constructed with C<< v2 => 1 >>, the request is dispatched to the v2 endpoint; otherwise the v1 endpoint is used.
 
 =cut
 
 sub call
+{
+    my ($self, $method, $params) = @_;
+
+    my $api_method = $self->{v2} ? '_call_v2' : '_call_v1';
+
+    return $self->$api_method($method, $params);
+}
+
+sub _call_v1
 {
     my ($self, $method, $params) = @_;
 
@@ -147,6 +123,11 @@ sub call
         try {
             $o = decode_json($q->decoded_content);
         } catch {       
+            if ($self->{debug})
+            {
+                warn $q->as_string;
+            }
+
             $o = { unprocessed => 1 };
         };
     }
@@ -156,6 +137,52 @@ sub call
         success => delete $o->{success} || 0,
     );
 }
+
+sub _call_v2
+{
+    my ($self, $method, $params) = @_;
+
+    my $site = $self->{site};
+
+    die "[Bizowie::API] fatal error: no method given" unless $method;
+
+    $params = $params || {};
+    $params->{api_key}     = $self->{api_key};
+    $params->{secret_key}  = $self->{secret_key};
+    $params->{api_version} = $self->{api_version} || '1.00'
+        unless $params->{api_version};
+
+    my $request = encode_json($params || { });
+
+    my $q = $self->{ua}->request(POST("https://${site}/bz/apiv2/call/$method",
+         Content_Type => 'form-data',
+         Content      => $request,
+        )
+    );
+
+    my $o;
+    {
+        local $SIG{__DIE__} = sub { };
+        try {
+            $o = decode_json($q->decoded_content);
+        } catch {       
+            $o = { unprocessed => 1 };
+
+            if ($self->{debug})
+            {
+                warn $q->as_string;
+            }
+        };
+    }
+
+    return WWW::Bizowie::API::Response->new(
+        data    => $o,
+        success => delete $o->{success} || 0,
+    );
+}
+
+
+
 
 =head1 DEPENDENCIES
 
@@ -167,7 +194,7 @@ Bizowie
 
 =head1 COPYRIGHT AND LICENSE
 
-Copyright (C) 2013-2018 Bizowie
+Copyright (C) 2013 Bizowie
 
 This library is free software; you can redistribute it and/or modify it under the same terms as Perl itself.
 
